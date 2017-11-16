1f4830f5af9bcf6a40bd8071a44c3304e1adbea5
Nerijus Bukartas <krekas@gmail.com>
2017-11-16 15:00:22 +0200

    User can view his own orders

---
+++ src/AppBundle/Controller/UserOrdersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Orders;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserOrdersController extends Controller
+{
+
+    /**
+     * @Route("/orders", name="users_orders")
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @internal param User $user
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Home:orders.html.twig', [
+            'orders' => $this->getUser()->getOrders()
+        ]);
+    }
+
+    /**
+     * @Route("/orders/{orderNumber}", name="orders_view")
+     * @param Orders $order
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function viewOrderAction(Orders $order)
+    {
+        return $this->render('AppBundle:Home:orders_view.html.twig', [
+            'order' => $order
+        ]);
+    }
+}
