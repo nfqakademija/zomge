41d151ad9a94facb9bab82662e5322c9dbd5239b
Nerijus Bukartas <krekas@gmail.com>
2017-11-27 10:02:25 +0200

    Pagination for users list

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -16,15 +16,25 @@ class UsersController extends Controller
 
     /**
      * @Route("/", name="admin_users_index")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $users = $this->getDoctrine()
             ->getRepository('AppBundle:User')
             ->findAll();
 
+        /** @var $paginator \Knp\Component\Pager\Paginator */
+        $paginator = $this->get('knp_paginator');
+        $result = $paginator->paginate(
+            $users,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', $this->getParameter('pagination_number'))
+        );
+
         return $this->render('AppBundle:Dashboard:users_index.html.twig', [
-            'users' => $users
+            'users' => $result
         ]);
     }
 
