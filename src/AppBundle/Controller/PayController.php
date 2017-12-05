7d6ddbd9db108f4b764c1ecf326c893932a4fa4c
Nerijus Bukartas <krekas@gmail.com>
2017-12-05 16:14:26 +0200

    Pay button

---
+++ src/AppBundle/Controller/PayController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Orders;
+use AppBundle\Security\OrderVoter;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PayController extends Controller
+{
+
+    /**
+     * @Route("/pay/{orderNumber}", name="pay_order")
+     * @param Orders $order
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function payAction(Orders $order)
+    {
+        $this->denyAccessUnlessGranted(OrderVoter::USER_ORDER, $order);
+
+        $em = $this->getDoctrine()->getManager();
+        $order->setStatus(2);
+        $em->flush();
+
+        $this->addFlash('success', 'Yay! Your order have been been.');
+
+        return $this->redirectToRoute('orders_view', ['orderNumber' => $order->getOrderNumber()]);
+    }
+}
